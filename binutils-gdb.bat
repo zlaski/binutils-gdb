a2934a85edcbb00b90d70a2c1d6f80c17c8d044a
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-01-03 12:10:51 -0800

    simply build slightly

--- binutils-gdb.bat
+++ binutils-gdb.bat
@@ -1,4 +1,8 @@
 @echo off
+
+:: we no longer use libiberty headers directly
+exit
+
 setlocal EnableExtensions EnableDelayedExpansion
 
 set OutDir=%~1
@@ -9,9 +13,9 @@ set QDIR="%OutDir%..\include\libiberty\"
 rmdir /s /q %QDIR% 2>nul
 mkdir %QDIR% 
 
-for /F %%H in (libiberty.lst) do (
-    echo %%H
-    xcopy /i /r /y /q %%H %QDIR%
+for /F %%H in (binutils-gdb.lst) do (
+    :: echo %%H
+    xcopy /i /r /y /q %%H %QDIR% >nul
 )
 
 pushd %QDIR%
